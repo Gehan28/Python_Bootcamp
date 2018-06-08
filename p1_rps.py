8157a68ba9867d1c4ac1c457bc9053ea34a3e2ce
Atom <gehanchopade28@gmail.com>
2018-06-08 22:21:58 +0530

    Autopep8 test

--- p1_rps.py
+++ p1_rps.py
@@ -1,43 +1,43 @@
-#libraries
+# libraries
 from random import randint
-#Code
+# Code
 
-l=['Rock(0)','Paper(1)','Scissors(2)']
+l = ['Rock(0)', 'Paper(1)', 'Scissors(2)']
 print("...Rock...")
 print("...Paper...")
 print("...Scissors...")
 print(l)
 
-status='y'
-while(status=='y'):
-	comp = randint(0,2) #generate random int
-	###
-	print("Choose Rock-Paper-Scissors")
-	user=int(input())
-	print('---------------------')
-	print("You:"+l[user])
-	print("Computer:"+l[comp])
-	print('---------------------')
-	print('')
-	print('*********************')
-	if(user==comp):
-		print('TIE GAME')
-	elif(user==0):
-		if(comp==2):
-			print('Winner Winner Chicken Dinner')
-		else:
-			print('Sore Loser')
-	elif(user==1):
-		if(comp==0):
-			print('Winner Winner Chicken Dinner')
-		else:
-			print('Sore Loser')
-	elif(user==2):
-		if(comp==1):
-			print('Winner Winner Chicken Dinner')
-		else:
-			print('Sore Loser')
-	print('*********************')
-	print('')
-	print('Play Again ? Yes-y No-n')
-	status=input()
+status = 'y'
+while(status == 'y'):
+    comp = randint(0, 2)  # generate random int
+
+    print("Choose Rock-Paper-Scissors")
+    user = int(input())
+    print('---------------------')
+    print("You:"+l[user])
+    print("Computer:"+l[comp])
+    print('---------------------')
+    print('')
+    print('*********************')
+    if(user == comp):
+        print('TIE GAME')
+    elif(user == 0):
+        if(comp == 2):
+            print('Winner Winner Chicken Dinner')
+        else:
+            print('Sore Loser')
+    elif(user == 1):
+        if(comp == 0):
+            print('Winner Winner Chicken Dinner')
+        else:
+            print('Sore Loser')
+    elif(user == 2):
+        if(comp == 1):
+            print('Winner Winner Chicken Dinner')
+        else:
+            print('Sore Loser')
+    print('*********************')
+    print('')
+    print('Play Again ? Yes-y No-n')
+    status = input()
--- pattern.py
+++ pattern.py
@@ -1,38 +1,36 @@
-#print('\U0001f600')
+# print('\U0001f600')
 print('Pattern')
-for i in range(1,11):
-	print('*'*i)
+for i in range(1, 11):
+    print('*'*i)
 
 print('------------------------------------------------------------------')
 print('Pattern')
-for i in range(10,0,-1):
-	print('*'*i)
+for i in range(10, 0, -1):
+    print('*'*i)
 
 print('------------------------------------------------------------------')
 print('Pattern')
-l=[]
-for x in range(0,11):
-	l.append('*')
-for i in range(0,6):
-	print(*l)
+l = []
+for x in range(0, 11):
+    l.append('*')
+for i in range(0, 6):
+    print(*l)
 
 print('------------------------------------------------------------------')
 print('Pattern')
-l=[]
-for x in range(0,11):
-	l.append('*')
-for i in range(0,6):
-	print(*l)
-	l[i]=l[10-i]=' '
+l = []
+for x in range(0, 11):
+    l.append('*')
+for i in range(0, 6):
+    print(*l)
+    l[i] = l[10-i] = ' '
 
 print('------------------------------------------------------------------')
 print('Pattern')
-l=[]
-for x in range(0,11):
-	l.append(' ')
-l[5]='*'
-for i in range(0,6):
-	l[5-i]=l[5+i]='*'
-	print(*l)
-
-
+l = []
+for x in range(0, 11):
+    l.append(' ')
+l[5] = '*'
+for i in range(0, 6):
+    l[5-i] = l[5+i] = '*'
+    print(*l)
